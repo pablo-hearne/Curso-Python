62435741c102987ddf66319fa7a17fe0d78d648d
Pablo-PC <pablohearne2@gmail.com>
2025-06-29 10:10:06 -0300

    Primera prueba

---
+++ Trabajo-practico-6/main.py
@@ -0,0 +1,19 @@
+"""1) Hacer un programa que gestiones datos para una escuela. El programa tiene que ser capaz
+de:
+a) Llevar un registro de todos los datos de alumnos de la escuela (Nombre, Apellido,
+fecha de nacimiento, DNI, Nombre de Tutor, registro de todas las notas, cantidad de
+faltas, cantidad de amonestaciones recibidas.)
+b) Mostrar los datos de cada alumno
+c) Modificar los datos de los alumnos
+d) Agregar alumnos
+e) Expulsar alumnos
+f) Dar Persistencia a los Datos del programa mediante la implementación Archivos
+El trabajo practico se deberá subir a un repositorio de GitHub Publico, y se entregara
+únicamente la dirección del repositorio (No de la pagina)."""
+
+
+
+
+
+
+
